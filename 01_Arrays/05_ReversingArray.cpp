c98fd346fd86b6ba2f81838338a4697cbf4406ec
Pavan Sarvesh <pavansarvesh31@gmail.com>
2024-06-18 19:22:52 +0530

    LinearSearch

--- 01_Arrays/05_ReversingArray.cpp
+++ 01_Arrays/05_ReversingArray.cpp
@@ -1,1 +1,26 @@
-#
+// todo: to reverse an array
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int reverse_array(vector<int> arr)
+{
+    int max_len = arr.size() - 1; // last index of the array
+    for (int i = 0; i < arr.size() / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[max_len];
+        arr[max_len] = temp;
+        max_len--;
+    }
+
+    for (int i : arr)
+        cout << i << " ";
+}
+
+int main()
+{
+    vector<int> array = {2, 3, 4, 5, 9};
+    reverse_array(array);
+}
