8a993e27e874956f86b5af084a0eb1ec2a53d511
pavansarvesh <pavansarvesh31@gmail.com>
2026-02-12 12:41:19 +0530

    code : leetcode 13

--- 04_strings/leetcode13.java
+++ 04_strings/leetcode13.java
@@ -1,8 +1,12 @@
 // 13. Roman to Integer
+
+import java.util.Map;
+import java.util.HashMap;
+
 class Solution {
     public int romanToInt(String s) {
         int sum = 0;
-        Map<Character ,Integer> romanMap = new HashMap<>();
+        Map<Character, Integer> romanMap = new HashMap<>();
         romanMap.put('I', 1);
         romanMap.put('V', 5);
         romanMap.put('X', 10);
@@ -12,7 +16,7 @@ class Solution {
         romanMap.put('M', 1000);
 
         for (int i = 0; i < s.length(); i++) {
-            if(i+1 < s.length() && romanMap.get(s.charAt(i)) < romanMap.get(s.charAt(i+1))){
+            if (i + 1 < s.length() && romanMap.get(s.charAt(i)) < romanMap.get(s.charAt(i + 1))) {
                 sum -= romanMap.get(s.charAt(i));
             } else {
                 sum += romanMap.get(s.charAt(i));
