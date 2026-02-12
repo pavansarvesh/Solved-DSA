d236848f964a9b0d36250abdcbdb83547cf5e154
pavansarvesh <pavansarvesh31@gmail.com>
2026-02-12 12:40:18 +0530

    code : leetcode 13

---
+++ 02_linkedList/code.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* next;
+
+        Node(int val) {
+            data = val;
+            next = NULL;
+        }
+};
+class List {
+    Node* head;
+    Node* tail;
+
+    public:
+        List() {
+            head = tail = NULL;
+        }
+
+        void push_front(int val) {
+            Node* newNode = new Node(val);
+            if(head == NULL) {
+                head = tail = newNode;
+                return;
+            } else {
+                newNode -> next = head;
+                head = newNode;
+            }
+        }
+
+        void push_back(int val) {
+            Node* newNode = new Node(val);
+            if(head == NULL){
+                head = tail = newNode;
+                return;
+            } else {
+                tail -> next = newNode;
+                tail = newNode;
+            }
+        }
+        void pop_front() {
+            if(head == NULL) {return;}
+            Node *temp = head;
+            head = head -> next;
+            temp -> next = NULL;
+            delete temp;
+        }
+        void pop_back() {
+            if(head == NULL) {return;}
+            Node *temp = head;
+            while(temp -> next != tail){
+                temp = temp
+            }
+        }
+        void printLL() {
+            // to print the ll
+            Node* temp = head;
+            while (temp != NULL){
+                cout << temp -> data << " -> ";
+                temp = temp -> next;
+            }
+            cout <<"NULL"<< endl;
+        }
+};
+
+int main(){
+    List ll;
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    ll.push_back(4);
+    ll.push_back(5);
+
+    ll.pop_front();
+
+    ll.printLL();
+    return 0;
+}
---
+++ 04_strings/leetcode13.java
@@ -0,0 +1,23 @@
+// 13. Roman to Integer
+class Solution {
+    public int romanToInt(String s) {
+        int sum = 0;
+        Map<Character ,Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        for (int i = 0; i < s.length(); i++) {
+            if(i+1 < s.length() && romanMap.get(s.charAt(i)) < romanMap.get(s.charAt(i+1))){
+                sum -= romanMap.get(s.charAt(i));
+            } else {
+                sum += romanMap.get(s.charAt(i));
+            }
+        }
+        return sum;
+    }
+}
